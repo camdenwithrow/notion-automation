43f7dba90dc9655cfc334c4ac5d11a15ba16b85d
Camden <cmwithrow@anderson.edu>
2022-02-04 13:01:57 -0500

    http request non 200 handling and comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import json
+import requests
 
 from notion import NotionApi
 
@@ -11,21 +12,44 @@ TODO_LIST_ID = os.getenv("TODO_LIST_ID")
 TOMORROW_LIST_ID = os.getenv("TOMORROW_LIST_ID")
 
 
+def check_request(resp):
+    """Checks request for 200 response"""
+    try:
+        # check resp status
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        # non 200 response
+        return "Error: " + str(e)
+
+
 def delete_checked(api):
+    """Deletes all todo blocks that are checked off in main todo list section"""
     todo_resp = api.get_block_children(TODO_LIST_ID, 100)
+    check_request(todo_resp)
+
+    # Load results from resp then find and delete all that are checked
     todo_results = json.loads(todo_resp.content)['results']
     to_delete = [
         obj["id"] for obj in todo_results
         if obj["type"] == "to_do" and obj["to_do"]["checked"]
     ]
     delete_resps = [api.delete_block(id) for id in to_delete]
+    for resp in delete_resps:
+        check_request(resp)
+
     return delete_resps
 
 
 def tomorrow_to_today(api):
+    """Moves all todo blocks in the tomorrow section to the main todo section 
+    then deletes them from tomorrow section"""
     tomorrow_resp = api.get_block_children(TOMORROW_LIST_ID, 100)
+    check_request(tomorrow_resp)
+
     tomorrow_results = json.loads(tomorrow_resp.content)["results"]
+    # check if there are any results to update
     if tomorrow_results is not []:
+        # create and updated blocks append to main todo section
         to_update = {
             "children": [
                 {
@@ -37,12 +61,16 @@ def tomorrow_to_today(api):
             ]
         }
         update_resp = api.append_block_children(TODO_LIST_ID, to_update)
+        check_request(update_resp)
 
+        # delete old blocks in tomorrow section
         to_delete = [res["id"] for res in tomorrow_results]
 
-        delete_resp = [api.delete_block(id) for id in to_delete]
+        delete_resps = [api.delete_block(id) for id in to_delete]
+        for resp in delete_resps:
+            check_request(resp)
 
-        return (update_resp, delete_resp)
+        return (update_resp, delete_resps)
     return None
 
 
