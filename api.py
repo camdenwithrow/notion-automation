f3948c56390e951915fbce3930c6104f827df05b
Camden <cmwithrow@anderson.edu>
2022-02-04 16:21:10 -0500

    incredibly simple auth

--- api.py
+++ api.py
@@ -1,5 +1,3 @@
-from email import message
-from multiprocessing.sharedctypes import Value
 from flask import Flask, jsonify, request, make_response
 import os
 from dotenv import load_dotenv
@@ -10,10 +8,20 @@ from notion import NotionApi
 
 load_dotenv()
 
+AUTH_KEY = os.getenv("AUTH_KEY")
 NOTION_API_KEY = os.getenv("NOTION_API_KEY")
 TODO_LIST_ID = os.getenv("TODO_LIST_ID")
 
 app = Flask(__name__)
+
+
+def create_resp(message: str, status: int, **kwargs):
+    return make_response(jsonify(message=message, status=status, **kwargs), status)
+
+
+def auth(headers):
+    return headers.get("Authorization") == "Bearer " + AUTH_KEY
+
 
 
 @app.route('/')
@@ -23,17 +31,14 @@ def index():
 
 @app.route('/health')
 def health():
+    if not auth(request.headers): return create_resp(message="Unauthorized", status=401)
     return make_response(jsonify(message="healthy"), 200)
-
-
-def create_resp(message:str, status:int, **kwargs):
-
-    return make_response(jsonify(message=message, status=status, **kwargs), status)
 
 
 @app.route('/quicktodo')
 def quicktodo():
     """add new todo into todo section from request"""
+    if not auth(request.headers): return create_resp(message="Unauthorized", status=401)
     api = NotionApi(NOTION_API_KEY)
     response = None
     try:
@@ -60,11 +65,13 @@ def quicktodo():
     except ValueError:
         response = create_resp(message="JSON parse error", status=400, code="ValueError")
     except KeyError:
-        response = create_resp(message="Request must include todo field", status=400, code="KeyError")
+        response = create_resp(
+            message="Request must include todo field", status=400, code="KeyError")
     except requests.exceptions.HTTPError as e:
         response = create_resp(message="Error: " + str(e), status=400)
     except Exception as e:
-        response = create_resp(message="Something went wrong " + str(e), status=400)
+        response = create_resp(
+            message="Something went wrong " + str(e), status=400)
     finally:
         return response
 
