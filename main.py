e726781ffad127bb147025e59e35b9ffef18dde0
Camden <cmwithrow@anderson.edu>
2022-02-04 12:18:51 -0500

    main setup

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os
+from dotenv import load_dotenv
+import json
+
+from notion import NotionApi
+
+load_dotenv()
+
+NOTION_API_KEY = os.getenv("NOTION_API_KEY")
+
 def main():
-    pass
+    api = NotionApi(NOTION_API_KEY)
 
 if __name__ == '__main__':
     main()
